77d8becec0b0f611b64bfe3897d9063a6599fc61
unn04012 <unn04012@naver.com>
2024-05-03 20:09:07 +0900

    update: updated router factory to instance

--- express-laboratory/src/common/router-factory.ts
+++ express-laboratory/src/common/router-factory.ts
@@ -5,15 +5,16 @@ import { ProductOrderController } from '../product/product-order.controller';
 export function registerRoutes(app: express.Application, controllers: any[]) {
   controllers.forEach((controller: any) => {
     const prefix = Reflect.getMetadata('path', controller);
-    const prototype = Object.getPrototypeOf(controller.prototype);
+
+    const prototype = Object.getPrototypeOf(controller);
+
     const methods = Object.getOwnPropertyNames(prototype);
-    console.log(ProductOrderController.prototype);
+
     methods.forEach((method: string) => {
       const path = Reflect.getMetadata('path', prototype, method);
       const httpMethod = Reflect.getMetadata('method', prototype, method);
       console.log(path, httpMethod);
       if (path && httpMethod) {
-        console.log(`${prefix}${path}`);
         app[httpMethod.toLowerCase()](`${prefix}${path}`, controller.prototype[method].bind(controller.prototype));
       }
     });
