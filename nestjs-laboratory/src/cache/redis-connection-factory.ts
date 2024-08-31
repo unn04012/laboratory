ad78115ea9780d411d5f71862ec140ea64075ffc
unn04012 <unn04012@naver.com>
2024-08-31 15:32:09 +0900

    update: added redis sentinel

--- nestjs-laboratory/src/cache/redis-connection-factory.ts
+++ nestjs-laboratory/src/cache/redis-connection-factory.ts
@@ -4,7 +4,16 @@ export function redisConnectionFactory() {
   const host = '127.0.0.1';
   const port = 3306;
 
-  const client = new Redis();
+  const client = new Redis({
+    sentinels: [
+      {
+        host,
+        port: 26379,
+      },
+    ],
+    name: 'mymaster',
+    role: 'master',
+  });
 
   return client;
 }
--- nestjs-laboratory/src/main.ts
+++ nestjs-laboratory/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { initializeTransactionalContext } from 'typeorm-transactional';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  initializeTransactionalContext();
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder().setTitle('Cats example').setDescription('The cats API description').setVersion('1.0').addTag('cats').build();
   const document = SwaggerModule.createDocument(app, config);
